e6f13a4da49c3f6e4dc1001778809189d6fdc169
MarsBighead <duanmhy@126.com>
2017-01-24 23:55:40 +0800

    Toger#A simple XML Unmarshal, an example for Go UT

---
+++ test/client.go
@@ -0,0 +1,63 @@
+package client 
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"os"
+	"time"
+)
+
+//func main() {
+//	Crawl("http://192.168.199.33:8001/m/api/matrix")
+//}
+
+// Crawl Send http request and get response data
+func Crawl(uri string) (*http.Response, error) {
+    host, _ := os.Hostname()
+    userAgent := "Cilent" + "/3.0" + " (Appcoach, " + host + ")"
+    fmt.Printf("userAgent:%s\n", userAgent)
+    proxyURL, _ := url.Parse("http://proxy.appcoachs.net:7070")
+        client := &http.Client{
+            Transport: &http.Transport{
+            Proxy: http.ProxyURL(proxyURL),
+        },
+        Timeout: time.Duration(300 * time.Second),
+    }
+    req, err := http.NewRequest("GET", uri, nil)
+    if err != nil{
+        return nil,err
+    }
+    req.Header.Set("User-Agent", userAgent)
+    resp, err := client.Do(req)
+    //resp, err := client.Get(uri)
+    if err != nil {
+        return nil, err
+    }
+    return resp, nil
+}
+
+// ReadResponse Get body data from http response
+func ReadResponse(resp *http.Response) ([]byte, error) {
+	if resp == nil {
+		return nil, errors.New("response is nil")
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	body = bytes.Replace(body, []byte("\x00"), []byte("\x20"), -1)
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("StatusCode: %v, %s", resp.StatusCode, string(body))
+	}
+
+	return body, nil
+}
---
+++ test/client_test.go
@@ -0,0 +1,7 @@
+package client 
+
+import "testing"
+
+func TestClient(t *testing.T) {
+    Crawl("http://127.0.0.1:8001/m/api/matrix")
+}

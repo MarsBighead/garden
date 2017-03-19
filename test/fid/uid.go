92ee9ef381b5475b94374a0b5fea85c4075a2836
MarsBighead <duanmhy@126.com>
2017-03-19 11:20:35 +0800

    Toger#Format hex(24) to string(36),  and reverse ops..

---
+++ test/fid/uid.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	fmt.Println("vim-go")
+	ExampleDecode()
+}
+func ExampleDecode() {
+	id := "117c786be2fc1ce33a8b456c"
+	//   "9223372036854775807"
+	src, err := hex.DecodeString(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var s string
+	for _, b := range src {
+		ss := strconv.Itoa(int(b))
+		if len(ss) <= 2 {
+			ss = "0" + ss
+		}
+		s += ss
+	}
+	fmt.Printf("s result %v, length of s: %v\n", s, len(s))
+	fmt.Printf("Origin hex %v\n", hex.EncodeToString(src))
+	rs := strings.Split(s, "")
+	var i int
+	for {
+		if i >= 12 {
+			break
+		}
+		st := rs[i*3] + rs[i*3+1] + rs[i*3+2]
+		fmt.Printf("Origin hex %v, TrimLeft  %v\n", st, strings.TrimLeft(st, "0"))
+		x, _ := strconv.Atoi(strings.TrimLeft(st, "0"))
+		fmt.Printf("number %x\n", x)
+		i++
+	}
+}

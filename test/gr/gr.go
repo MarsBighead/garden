693f174ea8e8013f739a6ef58cb6a55692603c28
MarsBighead <duanmhy@126.com>
2017-04-18 23:43:53 +0800

    Toger#Observe goroutine numbers

---
+++ test/gr/gr.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"time"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	c := make(chan bool, 10)
+	t := time.Tick(time.Second)
+
+	go func() {
+		for {
+			select {
+			case <-t:
+				watching()
+			}
+		}
+	}()
+
+	for i := 0; i < 10000000; i++ {
+		c <- true
+		go worker(i, c)
+	}
+
+	fmt.Println("Done")
+}
+
+func watching() {
+	fmt.Printf("Num Goroutine: %d\n", runtime.NumGoroutine())
+}
+
+func worker(i int, c chan bool) {
+	//fmt.Println("worker", i)
+	time.Sleep(100 * time.Microsecond)
+	<-c
+}

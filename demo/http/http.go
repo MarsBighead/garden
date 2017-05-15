ba4be2fc495b20f25d6fe02d6d287b7d1d8590f7
MarsBighead <duanmhy@126.com>
2017-05-15 23:56:36 +0800

    Toger#Move first http to demo

--- demo/http/http.go
+++ demo/http/http.go
@@ -5,7 +5,6 @@ import (
 	"garden/model"
 	"log"
 	"net/http"
-	"os"
 )
 
 func testHeader(w http.ResponseWriter, r *http.Request) {
@@ -18,7 +17,7 @@ func testHeader(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 }
-func testStatusNoContent(w http.ResponseWriter, req *http.Request) {
+func status204(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusNoContent)
 	fmt.Printf("Test 204 StatusCode\n")
 	//w.Write([]byte(""))
@@ -29,20 +28,12 @@ func say(w http.ResponseWriter, req *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", model.HttpHome)
+	http.HandleFunc("/", model.Home)
 	http.HandleFunc("/header", testHeader)
-	http.HandleFunc("/t", model.HttpTemp)
-	http.HandleFunc("/hello", testStatusNoContent)
+	http.HandleFunc("/tpl", model.HomeTemplate)
+	http.HandleFunc("/204", status204)
 	http.Handle("/handle", http.HandlerFunc(say))
 	log.Printf("Server is running on http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 	select {}
-}
-
-// checkError -Simplify error return checking
-func checkError(err error) {
-	if err != nil {
-		fmt.Println("Fatal error ", err.Error())
-		os.Exit(1)
-	}
 }

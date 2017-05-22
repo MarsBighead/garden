64e7a74134d1e04bfceecbe1c2866e09580323ce
MarsBighead <duanmhy@126.com>
2017-05-22 17:22:35 +0800

    Toger#Memory data compare

--- test/compare/compare.go
+++ test/compare/compare.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"fmt"
-	"garden/bank"
+	"garden/demo/bank"
+
 	"time"
 )
 
 func main() {
 	// Alice:
+	name := "Alice"
 	go func() {
-		bank.Deposit(200)
-		fmt.Println("=", bank.Balance())
+		bank.Deposit(200, name)
 	}()
+	time.Sleep(1 * time.Second)
 	// Bob:
-	go bank.Deposit(100)
+	name = "Bob"
+	go bank.Deposit(100, name)
 	delayTag(1000000 * time.Second)
 }
 

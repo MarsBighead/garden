617d0591deaada08d9a955871e75e267acf0937d
MarsBighead <duanmhy@126.com>
2017-10-15 19:55:55 +0800

    Build golang application by docker

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+GOBIN=$GOPATH/bin
+GOSRC=$GOPATH/src
+APP=$GOSRC/garden
+docker rm build 
+MBUILD_CID=$(docker create  -it \
+	-v $GOBIN:/go/bin \
+	-v $GOSRC:/go/src \
+	-v  $APP:/go/app \
+	--name build golang:1.8.3  \
+	/bin/bash )
+
+echo $MBUILD_CID
+docker start $MBUILD_CID
+
+#m=/Users/paul.duan/go/src/appcoachs.net/x
+#docker run --rm \
+#   -v $go:$go \
+#   -w "$(pwd)" golang:1.8.1 \
+#   cd $m/m \
+#   go build -v

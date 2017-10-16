303d1dbfef59900e7991b68a3279bbb217f1806d
MarsBighead <duanmhy@126.com>
2017-10-16 14:49:55 +0800

    Loop method besides for, while

---
+++ demo/ymaltest.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+var data = `
+blog: xiaorui.cc
+best_authors: ["fengyun","lee","park"]
+desc:
+  counter: 521
+  plist: [3, 4]
+`
+
+type T struct {
+	Blog    string
+	Authors []string `yaml:"best_authors,flow"`
+	Desc    struct {
+		Counter int   `yaml:"Counter"`
+		Plist   []int `yaml:",flow"`
+	}
+}
+
+func main() {
+	t := T{}
+	//把yaml形式的字符串解析成struct类型
+	err := yaml.Unmarshal([]byte(data), &t)
+	//修改struct里面的记录
+	t.Blog = "this is Blog"
+	t.Authors = append(t.Authors, "myself")
+	t.Desc.Counter = 99
+	fmt.Printf("--- t:\n%v\n\n", t)
+	//转换成yaml字符串类型
+	d, err := yaml.Marshal(&t)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	fmt.Printf("--- t dump:\n%s\n\n", string(d))
+}
---
+++ test/loop/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 0
+Loop:
+	fmt.Printf("%d\n", i)
+	if i < 10 {
+		i++
+		goto Loop
+	}
+}

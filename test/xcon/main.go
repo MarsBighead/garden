b99c40214acca4ed1097cf75f14359dd329be44a
MarsBighead <duanmhy@126.com>
2017-11-17 14:02:28 +0800

    Goroutine control method with wg or only channel

--- test/xcon/main.go
+++ test/xcon/main.go
@@ -2,35 +2,71 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 	"time"
 )
 
-func dgucoproducer(c chan int, max int) {
-	for i := 0; i < max; i++ {
-		c <- i
-	}
-	//close(c)
-}
-
-func dgucoconsumer(c chan int) {
-	ok := true
-	value := 0
-	for ok {
-		fmt.Println("Wait receive")
-		if value, ok = <-c; ok {
-			fmt.Println(value)
-		}
-		if ok == false {
-			fmt.Println("*******Break********")
-		}
-	}
-}
-
 func main() {
-	c := make(chan int)
-	defer close(c)
-	go dgucoproducer(c, 10)
-	go dgucoconsumer(c)
-	time.Sleep(time.Second * 5)
-	fmt.Println("Done")
+	threadNum := 4
+	length := 200
+	start := time.Now()
+	chChan(threadNum, length)
+	sp1 := time.Now()
+	fmt.Println("type 1 duration", sp1.Sub(start))
+	wgChan(threadNum, length)
+	sp2 := time.Now()
+	fmt.Println("type 2 duration", sp2.Sub(sp1))
+}
+
+func chChan(threadNum int, length int) {
+	var maxG int
+	done := make(chan bool)
+	defer close(done)
+	for n := 0; n < length; {
+		var i int
+		for i = 0; i < threadNum; i++ {
+			n++
+			if n >= length {
+				break
+			}
+			go func() {
+				done <- true
+			}()
+
+		}
+
+		// wait until both are done
+		for c := 0; c < i; c++ {
+			if c == 0 {
+				if runtime.NumGoroutine() > maxG {
+					maxG = runtime.NumGoroutine()
+				}
+			}
+			<-done
+		}
+	}
+	fmt.Println("max goroutine is", maxG)
+}
+func wgChan(threadNum int, length int) {
+	var maxG int
+	threadKey := make(chan int, threadNum)
+	var wg sync.WaitGroup
+	for n := 0; n < length; n++ {
+		wg.Add(1)
+		threadKey <- n
+		go func(i int) {
+			defer func() {
+				wg.Done()
+				<-threadKey
+			}()
+			if runtime.NumGoroutine() > maxG {
+				maxG = runtime.NumGoroutine()
+			}
+			//fmt.Println(time.Now(), "Goruntine number", runtime.NumGoroutine())
+		}(n)
+	}
+	wg.Wait()
+	close(threadKey)
+	fmt.Println("max goroutine is", maxG)
 }

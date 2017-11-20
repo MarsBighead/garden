a0a3e6b17e94a1c53e0c93ec677b66732cb034be
MarsBighead <duanmhy@126.com>
2017-11-20 09:53:11 +0800

    Pakage flag to accomplish some functions of Shell getopts

---
+++ demo/getopts/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func init() {
+	flag.Parse()
+}
+
+var lang = flag.String("lang", "golang", "the lang of the program")
+var age = flag.Int64("age", 18, "the age of the user")
+var safemod = flag.Bool("safemod", true, "whether using safemode")
+
+func main() {
+	//flag.Parse()
+	fmt.Println("lang is: ", *lang)
+	fmt.Println("age is: ", *age)
+	fmt.Println("safemod is: ", *safemod)
+}

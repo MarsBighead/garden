6a5130299181bfbcd2f805ffb15620d4c615a6dd
MarsBighead <duanmhy@126.com>
2018-08-29 22:18:46 +0800

    Add insert into Couchbase bucket with multiple goroutines

--- bio/cb_perf.go
+++ bio/cb_perf.go
@@ -2,11 +2,14 @@ package bio
 
 import (
 	"fmt"
+	"log"
+	"sync"
+	"time"
 
 	"github.com/couchbase/gocb"
 )
 
-func (s *Service) QueryAllRefGene(bucket *gocb.Bucket) error {
+func (s *Service) QueryAllRefGene(bucket *gocb.Bucket, ch chan *ResponseRefgene) error {
 	sStmt := fmt.Sprintf(`select name, chrom, strand, txStart, txEnd,
 	cdsStart, cdsEnd, exonCount, exonStarts, exonEnds, 
 	score, name2 gene, exonFrames 
@@ -24,9 +27,46 @@ func (s *Service) QueryAllRefGene(bucket *gocb.Bucket) error {
 		if err != nil {
 			return err
 		}
-		bucket.Upsert(fmt.Sprintf("%d", key), refGene, 0)
-
+		key++
+		refGene.DocKey = fmt.Sprintf("%d", key)
+		ch <- refGene
 		//fmt.Printf("%v\n", string(body))
 	}
 	return nil
+}
+
+func (s *Service) WriteBucket(bucket *gocb.Bucket, num int) {
+	ch := make(chan *ResponseRefgene, num)
+	done := make(chan bool)
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		s.QueryAllRefGene(bucket, ch)
+		close(ch)
+		for i := 0; i < num; i++ {
+			done <- true
+		}
+		close(done)
+	}()
+	for i := 0; i < num; i++ {
+		go func() {
+			var sig bool
+			for !sig {
+
+				select {
+				case v, ok := <-ch:
+					if ok {
+						_, err := bucket.Upsert(v.DocKey, v, 0)
+						if err != nil {
+							log.Println(err)
+						}
+					}
+				case sig = <-done:
+				}
+			}
+		}()
+	}
+	wg.Wait()
+	time.Sleep(10 * time.Second)
 }

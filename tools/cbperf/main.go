ca77df020724e7231d45772d69ffb9af4d846bf9
MarsBighead <duanmhy@126.com>
2018-08-30 11:02:38 +0800

    Connect to bucket configurable

--- tools/cbperf/main.go
+++ tools/cbperf/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
+	"fmt"
 	"garden/bio"
 	"garden/model"
+	"io/ioutil"
 	"log"
 	"os"
+	"time"
 
 	"github.com/couchbase/gocb"
 	_ "github.com/go-sql-driver/mysql"
@@ -28,18 +32,52 @@ func main() {
 	s := bio.Service{
 		DB: db,
 	}
-	cluster, err := gocb.Connect("couchbase://localhost")
+	body, err := ioutil.ReadFile(dir + "/config.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	cb := new(couchbase)
+	err = json.Unmarshal(body, cb)
+	if err != nil {
+		log.Fatal(err)
+	}
+	/*b, err := json.MarshalIndent(cb, "", "    ")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("config.json", b, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}*/
+
+	cluster, err := gocb.Connect(fmt.Sprintf("%s://%s", cb.Driver, cb.Host))
 	if err != nil {
 		log.Fatal(err)
 	}
 	cluster.Authenticate(gocb.PasswordAuthenticator{
-		Username: "root",
-		Password: "togerme",
+		Username: cb.Username,
+		Password: cb.Password,
 	})
-	bucket, err := cluster.OpenBucket("refgene", "")
+	bucket, err := cluster.OpenBucket(cb.Bucket, "")
 	if err != nil {
 		log.Fatal(err)
 	}
+	err = bucket.Manager(cb.Username, cb.Password).Flush()
+	if err != nil {
+		log.Fatal(err)
+	}
+	time.Sleep(10 * time.Second)
 	s.WriteBucket(bucket, 1000)
 
+}
+
+type couchbase struct {
+	Driver   string `yaml:"driver"    json:"driver"`
+	Host     string `yaml:"host"      json:"host"`
+	Username string `yaml:"username"  json:"username"`
+	Password string `yaml:"password"  json:"password"`
+	Bucket   string `yaml:"bucket"    json:"bucket"`
+	Switch   string `yaml:"switch"    json:"switch"`
+	Port     string `yaml:"-"         json:"-"`
 }

990f714d78fb0fc5b72cb8138d21e5d91423b11f
MarsBighead <duanmhy@126.com>
2019-04-04 13:32:57 +0800

    Make filename soudly

--- demo/argments/main.go
+++ demo/argments/main.go
@@ -46,3 +46,18 @@ func main() {
 	fmt.Println("h:", host)
 	fmt.Println("tail:", flag.Args())
 }
+
+func init() {
+	flag.Parse()
+}
+
+var lang = flag.String("lang", "golang", "the lang of the program")
+var age = flag.Int64("age", 18, "the age of the user")
+var safemod = flag.Bool("safemod", true, "whether using safemode")
+
+func getOpts() {
+	//flag.Parse()
+	fmt.Println("lang is: ", *lang)
+	fmt.Println("age is: ", *age)
+	fmt.Println("safemod is: ", *safemod)
+}
